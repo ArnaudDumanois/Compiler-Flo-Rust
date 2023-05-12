582beb4c1757df2602eff8471a5e3e1c8c344c2a
arnaud dumanois <arnaud.dumanois@etu.unice.fr>
2023-05-12 23:21:55 +0200

    first implementation for the lexer

--- src/analyse_lexicale.rs
+++ src/analyse_lexicale.rs
@@ -4,71 +4,74 @@ Analyseur lexical du langage Flo
 
 use regex::Regex;
 
-pub enum Token {
-    // Identificateur et Entier
-    Ident(String),
-    Entier(i32),
-
-    // opérateurs
+#[derive(Debug)]
+pub enum Op {
     Plus,
     Moins,
     Mult,
     Div,
 }
 
-pub fn lexer(input: &str) -> Vec<Token> {
+#[derive(Debug)]
+pub enum Token {
+    // Identificateur et Entier
+    Ident(String),
+    Entier(i32),
 
-    let mut tokens = vec![];
-    let regex_ident = Regex::new(r"^[a-zA-Z][a-zA-Z0-9]*$").unwrap();
-    let regex_entier = Regex::new(r"^[0-9]+$").unwrap();
-    let operateur = ['+', '-', '*', '/'];
-    let regex_space = Regex::new(r"^\s+$").unwrap();
-
-    let mut remaining_input = input;
-    while !remaining_input.is_empty() {
-        // ignorer les espaces
-        /*
-        if let Some(c) = remaining_input.chars().next() {
-            if c.is_whitespace() {
-                remaining_input = remaining_input.trim_start();
-                continue;
-            }
-        }
-         */
-        if let Some(captures) = regex_space.captures(remaining_input) {
-            remaining_input = &remaining_input[captures.get(0).unwrap().end()..];
-            continue;
-        }
-
-        if let Some(captures) = regex_ident.captures(remaining_input) {
-            let ident = captures.get(0).unwrap().as_str();
-            tokens.push(Token::Ident(ident.to_string()));
-            remaining_input = &remaining_input[captures.get(0).unwrap().end()..];
-            continue;
-        }
-
-        if let Some(captures) = regex_entier.captures(remaining_input) {
-            let entier = captures.get(0).unwrap().as_str();
-            tokens.push(Token::Entier(entier.parse::<i32>().unwrap()));
-            remaining_input = &remaining_input[captures.get(0).unwrap().end()..];
-            continue;
-        }
-
-        // on vérifie si le caractère est un opérateur
-        if operateur.contains(&remaining_input.chars().next().unwrap()) {
-            match remaining_input.chars().next().unwrap() {
-                '+' => tokens.push(Token::Plus),
-                '-' => tokens.push(Token::Moins),
-                '*' => tokens.push(Token::Mult),
-                '/' => tokens.push(Token::Div),
-                _ => panic!("Unexpected character: {}", remaining_input.chars().next().unwrap()),
-            }
-            remaining_input = &remaining_input[1..];
-            continue;
-        }
-
-        panic!("Unexpected character: {}", remaining_input.chars().next().unwrap());
-    }
-    tokens
+    // opérateurs
+    Operator(Op),
 }
+
+
+// Fonction lexer
+pub fn lexer(mut code: &str) -> Vec<Token> {
+
+    let regex_entier = Regex::new(r"^[0-9]+").unwrap();
+    let regex_ident = Regex::new(r"^[a-zA-Z][a-zA-Z0-9]*").unwrap();
+
+    let mut res = Vec::new();
+    loop {
+        code = code.trim_start();
+
+        if code.is_empty() {
+            break;
+        }
+        if let Some(m) = regex_entier.find(code) {
+            res.push(Token::Entier(m.as_str().parse().unwrap()));
+            code = &code[m.end()..];
+        } else if let Some(m) = regex_ident.find(code) {
+            res.push(Token::Ident(m.as_str().to_owned()));
+            code = &code[m.end()..];
+        } else {
+            let c = code.chars().next().unwrap();
+            match c {
+                '+' => res.push(Token::Operator(Op::Plus)),
+                '-' => res.push(Token::Operator(Op::Moins)),
+                '*' => res.push(Token::Operator(Op::Mult)),
+                '/' => res.push(Token::Operator(Op::Div)),
+                _ => panic!("Caractère inconnu: {}", c)
+            };
+            code = &code[1..];
+        }
+    }
+    res
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,9 @@ mod generation_code;
 
 fn main() {
 
-    let input = "1";
-    let tokens = analyse_lexicale::lexer(input.trim());
-
-
-    for token in tokens {
-        match token {
-            analyse_lexicale::Token::Entier(valeur) => println!("Entier: {}", valeur),
-            analyse_lexicale::Token::Ident(ident) => println!("Ident: {}", ident),
-            analyse_lexicale::Token::Plus => println!("Plus"),
-            analyse_lexicale::Token::Moins => println!("Moins"),
-            analyse_lexicale::Token::Mult => println!("Mult"),
-            analyse_lexicale::Token::Div => println!("Div"),
-        }
-    }
+    let input = "1 + 1";
+    let tokens = analyse_lexicale::lexer(input);
+    println!("Tokens: {:?}", tokens);
 
 
 }
